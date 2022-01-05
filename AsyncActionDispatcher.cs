724097a0bf7f2274ee278872f2c75c82a3c02741
TexelProto <stinyal@protonmail.com>
2022-01-05 15:14:58 +0100

    Initial commit

---
+++ AsyncActionDispatcher.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Threading.Tasks;
+
+namespace Protos
+{
+	public struct Unit { }
+
+	public class AsyncActionDispatcher : AsyncEventDispatcher<Action, Unit>
+	{
+		public void Invoke() => this.Invoke( default );
+		public Task InvokeAsync() => this.InvokeAsync( default );
+
+		public void InvokeReverse() => this.InvokeReverse( default );
+		public Task InvokeReverseAsync() => this.InvokeReverseAsync( default );
+
+		protected override void InvokeListener(Action del, Unit data)
+		{
+			del();
+		}
+	}
+}
---
+++ AsyncEventDispatcher.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Protos
+{
+	public abstract class AsyncEventDispatcher<TDel, TData>
+		where TDel : Delegate
+	{
+		[ThreadStatic] private static List<TDel> delegateCopies;
+
+		private readonly List<TDel> listeners = new();
+
+		public void Register(TDel callback)
+		{
+			lock (this.listeners)
+				this.listeners.Add( callback );
+		}
+
+		public void Unregister(TDel callback)
+		{
+			lock (this.listeners)
+				this.listeners.Remove( callback );
+		}
+
+		public void Clear()
+		{
+			lock (this.listeners)
+				this.listeners.Clear();
+		}
+
+		public void Invoke(TData data)
+		{
+			delegateCopies ??= new List<TDel>();
+			delegateCopies.Clear();
+			lock (this.listeners)
+				delegateCopies.AddRange( this.listeners );
+
+			this.InvokeAll( data, 0, delegateCopies.Count, 1 );
+		}
+
+		public void InvokeReverse(TData data)
+		{
+			delegateCopies ??= new List<TDel>();
+			delegateCopies.Clear();
+			lock (this.listeners)
+				delegateCopies.AddRange( this.listeners );
+
+			this.InvokeAll( data, delegateCopies.Count - 1, -1, -1 );
+		}
+
+		private void InvokeAll(TData data, int start, int end, int change)
+		{
+			List<Exception> exceptions = null;
+			for (var i = start; i != end; i += change)
+			{
+				try
+				{
+					this.InvokeListener( delegateCopies[i], data );
+				}
+				catch (Exception e)
+				{
+					exceptions ??= new List<Exception>();
+					exceptions.Add( e );
+				}
+			}
+
+			if (exceptions != null)
+				throw new AggregateException( exceptions );
+		}
+
+		public async Task InvokeAsync(TData data)
+		{
+			await Task.Yield();
+			this.Invoke( data );
+		}
+
+		public async Task InvokeReverseAsync(TData data)
+		{
+			await Task.Yield();
+			this.InvokeReverse( data );
+		}
+
+		protected abstract void InvokeListener(TDel del, TData data);
+	}
+}
